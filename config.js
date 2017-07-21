797c2f37dd92a91f169a8af3feb827208728576a
Steve Domenico <stratcat944@gmail.com>
2017-07-21 15:02:37 -0600

    anti-Trump Twitterbot

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'wpCyzCMw6TNgVId5qL2D5WaWQ',
+	consumer_secret: 'LKNEZWVq7oxSaNaQnUIP4anp4XVPiyBS98DdvrsmfGB5P9QtxV',
+	access_token_key: '888191713130446848-gozWErFRHO3zJrllgif0NKwWnZ55rh2',
+	access_token_secret: 'VVCozbb334vmGS4pAs8TvNbXeKWWy24d0ivCr58pLpBAy'
+}
